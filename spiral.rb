f1a288783a1e5b8446a170b4e601d1e9f9121e8c
Daisuke Shimamoto <diskshima@gmail.com>
2019-04-30 14:29:44 +0900

    Add spiral dataset generation

---
+++ spiral.rb
@@ -0,0 +1,30 @@
+require 'numo/narray'
+
+def load_data(seed=1984)
+  Numo::NArray.srand(seed)
+  n = 100
+  dim = 2
+  cls_num = 3
+
+  x = Numo::DFloat.zeros(n * cls_num, dim)
+  t = Numo::Int32.zeros(n * cls_num, cls_num)
+
+  cls_num.times.each do |j|
+    n.times.each do |i|
+      rate = i.to_f / n
+      radius = 1.0 * rate
+      theta = j * 4.0 + 4.0 * rate + Numo::DFloat.new.rand_norm * 0.2
+
+      ix = n * j + i
+      x[ix, true] = [radius * Numo::DFloat::Math.sin(theta),
+                     radius * Numo::DFloat::Math.cos(theta)]
+      t[ix, j] = 1
+    end
+  end
+
+  [x, t]
+end
+
+if __FILE__ == $0
+  p load_data
+end
