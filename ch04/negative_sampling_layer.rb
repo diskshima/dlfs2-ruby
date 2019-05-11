f4f8525452603b7a035949833068739f60d27671
Daisuke Shimamoto <diskshima@gmail.com>
2019-05-11 20:10:55 +0900

    Add UnigramSampler

--- ch04/negative_sampling_layer.rb
+++ ch04/negative_sampling_layer.rb
@@ -28,3 +28,46 @@ class EmbeddingDot
     dh
   end
 end
+
+class UnigramSampler
+  def initialize(corpus, power, sample_size)
+    @sample_size = sample_size
+    @vocab_size = nil
+    @word_p = nil
+
+    counts = {}
+
+    corpus.each do |word_id|
+      counts[word_id] = counts[word_id] ? 1 : counts[word_id] + 1
+    end
+
+    vocab_size = counts.length
+    @vocab_size = vocab_size
+
+    @word_p = Numo::NFloat.zeros(vocab_size)
+
+    vocab_size.times do |i|
+      @word_p[i] = counts[i]
+    end
+
+    @word_p = @word_p ** power
+    @word_p = @word_p.sum
+  end
+
+  def get_negative_sample(target)
+    batch_size = target.sample[0]
+
+    negative_sample = Numo::UInt32.zeros(batch_size, @sample_size)
+
+    batch_size.times do |i|
+      p = @word_p.copy
+      target_idx = target[i]
+      p[target_idx] = 0
+      p /= p.sum
+      negative_sample[i, true] =
+        random_choice(vocab_size, size: @sample_size, p: p)
+    end
+
+    negative_sample
+  end
+end
