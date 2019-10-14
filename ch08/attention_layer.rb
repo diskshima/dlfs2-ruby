00b3184d6152ee129341e15fd3e8ea6de49bbd78
Daisuke Shimamoto <diskshima@gmail.com>
2019-10-14 14:12:15 +0900

    Add Attention layers

--- ch08/attention_layer.rb
+++ ch08/attention_layer.rb
@@ -72,3 +72,70 @@ class AttentionWeight
     [dhs, dh]
   end
 end
+
+# Attention layer
+class Attention
+  def initialize
+    @params = []
+    @grads = []
+
+    @attention_weight_layer = AttentionWeight.new
+    @weight_sum_layer = WeightSum.new
+    @attention_weight = nil
+  end
+
+  def forward(hs, h)
+    a = @attention_weight_layer.forward(hs, h)
+    out = @weight_sum_layer.forward(hs, a)
+    @attention_weight = a
+    out
+  end
+
+  def backward(dout)
+    dhs0, da = @weight_sum_layer.backward(dout)
+    dhs1, dh = @attention_weight_layer.backward(da)
+    dhs = dhs0 + dhs1
+    [dhs, dh]
+  end
+end
+
+# Attention layer for multiple blocks.
+class TimeAttention
+  def initialize
+    @params = []
+    @grads = []
+    @layers = nil
+    @attention_weights = nil
+  end
+
+  def forward(hs_enc, hs_dec)
+    _n, t, _h = hs_dec.shape
+    out = Numo::SFloat.zeros(hs_dec.shape)
+    @layers = []
+    @attention_weights = []
+
+    t.times do |ti|
+      layer = Attention.new
+      out[true, ti, true] = layer.forward(hs_enc, hs_dec[true, ti, true])
+      @layers.append(layer)
+      @attention_weights.append(layer.attention_weight)
+    end
+
+    out
+  end
+
+  def backward(dout)
+    _n, t, _h = dout.shape
+    dhs_enc = 0
+    dhs_dec = Numo::SFloat.zeros(dout.shape)
+
+    t.times do |ti|
+      layer = @layers[ti]
+      dhs, dh = layer.backward(dout[true, ti, true])
+      dhs_enc += dhs
+      dhs_dec[true, ti, true] = dh
+    end
+
+    [dhs_enc, dhs_dec]
+  end
+end
